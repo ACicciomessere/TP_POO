094f88beb2640a3cda39f725f58d583aa6455215
ACicciomessere <108630511+ACicciomessere@users.noreply.github.com>
2023-07-04 20:41:53 -0300

    Update Ellipse.java

--- src/backend/model/Ellipse.java
+++ src/backend/model/Ellipse.java
@@ -52,4 +52,12 @@ public class Ellipse extends Figure {
         return sMinorAxis;
     }
 
+    @Override
+    public Figure selected(Point startPoint, Point endPoint){
+        Point centerPoint = new Point(Math.abs(endPoint.getX() + startPoint.getX()) / 2, (Math.abs((endPoint.getY() + startPoint.getY())) / 2));
+		double sMayorAxis = Math.abs(endPoint.getX() - startPoint.getX());
+		double sMinorAxis = Math.abs(endPoint.getY() - startPoint.getY());
+		return new Ellipse(centerPoint, sMayorAxis, sMinorAxis);
+    }
+
 }
