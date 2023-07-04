7bc1c47337b7e3396d9540d9138c0b0622b27063
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-07-04 14:21:50 -0300

    Update Ellipse.java
    Se modifico la accesibilidad de las variables centerPoint, sMayorAxis, sMinorAxis a private. Se implementa el metodo pointBelongs

--- src/backend/model/Ellipse.java
+++ src/backend/model/Ellipse.java
@@ -2,8 +2,8 @@ package backend.model;
 
 public class Ellipse extends Figure {
 
-    protected final Point centerPoint;
-    protected final double sMayorAxis, sMinorAxis;
+    private final Point centerPoint;
+    private final double sMayorAxis, sMinorAxis;
 
     public Ellipse(Point centerPoint, double sMayorAxis, double sMinorAxis) {
         this.centerPoint = centerPoint;
@@ -14,6 +14,12 @@ public class Ellipse extends Figure {
     @Override
     public String toString() {
         return String.format("Elipse [Centro: %s, DMayor: %.2f, DMenor: %.2f]", centerPoint, sMayorAxis, sMinorAxis);
+    }
+
+    @Override
+    public boolean pointBelongs(Point eventPoint) {
+        return ((Math.pow(eventPoint.getX() - getCenterPoint().getX(), 2) / Math.pow(getsMayorAxis(), 2)) +
+                (Math.pow(eventPoint.getY() - getCenterPoint().getY(), 2) / Math.pow(getsMinorAxis(), 2))) <= 0.30;
     }
 
     public Point getCenterPoint() {
