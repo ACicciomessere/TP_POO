c3f363cf1f19bb7d9bd1db7a18763a3529a9cb0c
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-07-04 14:49:45 -0300

    Update Figure.java
    Se agregan los metodos abstractos fill y stroke para que el metodo redrawCanvas de la clase PaintPane tenga un estilo mas adecuado al paradigma de POO

--- src/backend/model/Figure.java
+++ src/backend/model/Figure.java
@@ -2,4 +2,6 @@ package backend.model;
 
 public abstract class Figure {
     public abstract boolean pointBelongs(Point eventPoint);
+    public abstract void fill(GraphicsContext gc);
+    public abstract void stroke(GraphicsContext gc);
 }
