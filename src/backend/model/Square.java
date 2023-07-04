826e83d3cc6b4cbf1db5ecf89d736c3e4c2ce5e1
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-07-04 13:31:28 -0300

    Update Square.java
    Se corrigio que se accediera a las variables x e y de la instancia de Point utilizando los getters correspondientes

--- src/backend/model/Square.java
+++ src/backend/model/Square.java
@@ -1,14 +1,12 @@
 package backend.model;
 
 public class Square extends Rectangle {
-
     public Square(Point topLeft, double size) {
-        super(topLeft, new Point(topLeft.x + size, topLeft.y + size));
+        super(topLeft, new Point(topLeft.getX() + size, topLeft.getY() + size));
     }
 
     @Override
     public String toString() {
-        return String.format("Cuadrado [ %s , %s ]", topLeft, bottomRight);
+        return String.format("Cuadrado [ %s , %s ]", getTopLeft(), getBottomRight());
     }
-
 }
