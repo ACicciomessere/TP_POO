d2d57c0d755e325f9338b79eeac9c630e08ec3b2
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-07-04 14:22:28 -0300

    Update Circle.java
    Se hace la implementacion de pointBelongs

--- src/backend/model/Circle.java
+++ src/backend/model/Circle.java
@@ -1,17 +1,22 @@
 package backend.model;
 
 public class Circle extends Ellipse {
-
     private final double radius;
 
     public Circle(Point centerPoint, double radius) {
-        super(centerPoint, 2 * radius, 2 * radius);
+        super(centerPoint, 2 * radius, 2 * radius); // Revisar si esto esta bien
         this.radius = radius;
     }
 
     @Override
     public String toString() {
-        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, radius);
+        return String.format("Círculo [Centro: %s, Radio: %.2f]", getCenterPoint(), radius);
+    }
+
+    @Override
+    public boolean pointBelongs(Point eventPoint) {
+        return Math.sqrt(Math.pow(getCenterPoint().getX() - eventPoint.getX(), 2) +
+                Math.pow(getCenterPoint().getY() - eventPoint.getY(), 2)) < getRadius();
     }
 
     public double getRadius() {
