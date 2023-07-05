dd3cc7290a7db6096a53fedf944fbd86f6ef64c8
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2023-07-04 22:53:38 -0300

    Update CanvasState.java
    Se agrega una coleccion Map que va a tener como key la layer correspondiente y como value una coleccion con las figuras de esa layer. Se corrige el funcionamiento de figures. Falta adecuar el funcionamiento de deleteFigure

--- src/backend/CanvasState.java
+++ src/backend/CanvasState.java
@@ -3,13 +3,24 @@ package backend;
 import backend.model.Figure;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CanvasState {
 
     private final List<Figure> list = new ArrayList<>();
 
-    public void addFigure(Figure figure) {
+    private Map<String, List<Figure>> layersFigures = new HashMap<>(); // El key es la layer, y el value son las figures de la layer
+
+    public void addFigure(String layer, Figure figure) {
+        if(layer == null) {
+            throw new IllegalArgumentException("Selecciona una capa"); // Aca hay que poner un alert de seleccionar una capa
+        }
+        if(!layersFigures.containsKey(layer)) {
+            layersFigures.put(layer, new ArrayList<>());
+        }
+        layersFigures.get(layer).add(figure);
         list.add(figure);
     }
 
@@ -17,8 +28,13 @@ public class CanvasState {
         list.remove(figure);
     }
 
-    public Iterable<Figure> figures() {
-        return new ArrayList<>(list);
+    public Iterable<Figure> figures(List<String> layers) {
+        List<Figure> toReturn = new ArrayList<>();
+        for(String layer : layers) {
+            if(layersFigures.containsKey(layer)) {
+                toReturn.addAll(layersFigures.get(layer));
+            }
+        }
+        return toReturn;
     }
-
 }
