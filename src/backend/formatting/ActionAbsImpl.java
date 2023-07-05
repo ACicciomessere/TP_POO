b73cdaa5c4af998322ea743de4a158f1f8c12c43
ACicciomessere <108630511+ACicciomessere@users.noreply.github.com>
2023-07-05 19:42:37 -0300

    Update ActionAbsImpl.java

--- src/backend/formatting/ActionAbsImpl.java
+++ src/backend/formatting/ActionAbsImpl.java
@@ -1,6 +1,10 @@
 public abstract class ActionAbsImpl includes Action{
   private Figure figure;
 
+  public Figure(Figure figure){
+    this.figure = figure;
+  }
+  
   public abstract void undoAction();
   public abstract void activateAction();
   
