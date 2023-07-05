cc95f2d31181ef36afeea945a49ec93e36baa4e4
ACicciomessere <108630511+ACicciomessere@users.noreply.github.com>
2023-07-05 19:53:57 -0300

    Update CopyFigure.java

--- src/backend/formatting/CopyFigure.java
+++ src/backend/formatting/CopyFigure.java
@@ -3,7 +3,7 @@ package backend.formatting;
 import backend.model.Figure;
 import backend.model.Point;
 
-public class CopyFigure extends FormattingActions {
+public class CopyFigure extends ActionAbsImpl {
 
     private Figure copyFigure;
     private final Point canvasCenter= new Point(400, 300);
